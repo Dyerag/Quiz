980fe46f259d8f363550f0dbdd78b599b99c519d
dea <dea@PROELE78>
2024-01-10 13:27:13 +0100

    Add project files.

---
+++ GUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    static public class GUI
+    {
+        static public void Print(string text, int height, int width,ConsoleColor color = ConsoleColor.White, int pause = 0)
+        {
+            Console.SetCursorPosition(width, height);
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.White;
+            exit(pause); 
+        }
+
+        static public void Print(string text, int pause = 0)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(15, 2);
+            Console.Write(text);
+            exit(pause);
+        }
+
+        private static void exit(int pause)
+        {
+            if (pause == 0)
+            {
+                Console.ReadKey();
+            }
+            else
+            {
+                Thread.Sleep(pause);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Principal;
+
+
+namespace Quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            GUI.Print("Det her er en Quiz om emnet: ");
+            Thread.Sleep(3000);
+            //todo add the Ability to see what the questions in the json file is about, to desplay here
+            Theme("Vandforbrug!");
+            GUI.Print("Quizen har 6 spørgsmål");
+            
+
+
+
+        }
+
+        static private void Theme(string name)
+        {
+            int height = Console.GetCursorPosition().Top, width = Console.GetCursorPosition().Left;
+            for (int i = 0; i < 20; i++)
+            {
+                GUI.Print(name,height,width, ConsoleColor.Green);
+                Thread.Sleep(100);
+                GUI.Print(name,height,width, ConsoleColor.Yellow);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
