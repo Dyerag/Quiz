38e5204288e14ffbd27d6a4bdb283acc00b9b3c5
dea <dea@PROELE78>
2024-01-12 10:36:58 +0100

    Added some comments

--- IO.cs
+++ IO.cs
@@ -8,14 +8,19 @@ namespace Quiz
 {
     public class IO
     {
+        // This field is technically reduntant, as the code make get the directory,
+        // could have either been attached to the property, or put in the constructor.
         private string _filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Questions");
 
-        public string? FilePath { get =>_filePath; }
+        public string? FilePath { get => _filePath; }
 
         public IO()
         {
-            if(!File.Exists(FilePath))
+            if (!File.Exists(FilePath))
+            {
+                //todo notify user that the folder has been made, and they need to put a file in it. Aswell as what to name th file
                 Directory.CreateDirectory(FilePath);
+            }
         }
     }
 }
