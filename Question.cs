e035bd6e27e98191643d06c9a6c466930f808d49
dea <dea@PROELE78>
2024-01-12 10:33:28 +0100

    Made all the properties for the class, although, need to do something about Theme.

--- Question.cs
+++ Question.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Quiz
 {
-    internal class Question
+    public class Question
     {
+        //todo move the theme field elsewhere
+        //gonna have to alter it aswell, but havent figured out how to get the theme of the quiz, as its from the same json file
+        public string? Theme { get; }
+        public string? Inquery { get; }
+        public string? RightChoice { get; }
+        public string? WrongChoice { get; }
+        public List<string>? Options { get; }
+        public string? Answer { get; }
+        public string? Explanation { get; }
+
     }
 }
