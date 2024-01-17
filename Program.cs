a66cfc704321d438fc171264a21c32e19fba47db
dea <dea@PROELE78>
2024-01-17 12:56:31 +0100

    The methods Theme and intro were moved back to the program class, as i felt they didn' t fit in the Game class. They were therefore made static. I also need to stop forgetting about branches, and end op writing in master.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 
 namespace Quiz
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -13,19 +13,53 @@ namespace Quiz
             //todo move som of the prints here, to the constructor in game
             GUI.Print("Det her Quiz program får alle spørgsmålene og svarende,");
             Thread.Sleep(2000);
-            GUI.Print("ved at indlæse json filen der lægges i den givne folder.",0,2000);
+            GUI.Print("ved at indlæse json filer der lægges i den givne folder.", 0, 2000);
             GUI.Print("Hvis du gerne vil bruge en anden quiz i dette program,", 0, 2000);
-            GUI.Print("skal du bare erstatte json filen i folderen, med den nye quiz.",0,2000);
-            GUI.Print("Også i json format, og opsat på samme måde", 0);
+            GUI.Print("skal du bare erstatte json filen i folderen, med den nye quiz.", 0, 2000);
+            GUI.Print("Også i json format, og opsat på samme måde.", 0);
             #endregion
 
             #region How to play
-            GUI.Print("Du vælger dit svar ved at trykke A, B, eller C.",0);
-            GUI.Print("Alternativt kan du også bruge 1, 2, eller 3.", 0);
+            GUI.Print("Du vælger dit svar ved at trykke A, B, eller C.");
+            Thread.Sleep(2000);
+            GUI.Print("Alternativt kan du også bruge 1, 2, eller 3.", 0, 2000);
             GUI.Print("Når du er klar, skal du bare trykke noget for at begynde", 0);
             #endregion
 
-            Game.Play();
+            do
+            {
+                new Game();
+            } while (true);
         }
+
+        /// <summary>
+        /// Tells the users the Theme and number of questions
+        /// </summary>
+        public static void Intro(QuestionList gameQuestions)
+        {
+            GUI.Print("Det her er en Quiz om emnet: ");
+            Thread.Sleep(3000);
+            Theme(gameQuestions.Theme);
+
+            GUI.Print($"Quizzen har {gameQuestions.Questions.Count} spørgsmål.", 0, 3000);
+            GUI.Print("Klar?", 0);
+        }
+
+        /// <summary>
+        /// Gives the quiz theme the flicker
+        /// </summary>
+        /// <param name="name"></param>
+        private static void Theme(string name)
+        {
+            int height = Console.GetCursorPosition().Top, width = Console.GetCursorPosition().Left;
+
+            for (int i = 0; i < 15; i++)
+            {
+                GUI.Print(name, height, width, 100, ConsoleColor.Red);
+                GUI.Print(name, height, width, 100, ConsoleColor.Yellow);
+            }
+        }
+
+
     }
 }
