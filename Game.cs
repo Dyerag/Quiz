d38768c4f814c745cd7f43b92fb14845ac476b55
dea <dea@PROELE78>
2024-01-17 12:56:31 +0100

    The methods Theme and intro were moved back to the program class, as i felt they didn' t fit in the Game class. They were therefore made static. I also need to stop forgetting about branches, and end op writing in master.

--- Game.cs
+++ Game.cs
@@ -17,40 +17,19 @@ namespace Quiz
         {
             IO setup = new();
             GameQuestions = setup.FileFetch();
+            var text = setup.FileFetch();
+            Console.WriteLine(text);
+            Program.Intro(GameQuestions);
+            Play();
 
         }
-        // todo add a constructor, that writes the stuff from the beginning of the code, most likely by pointing to another method
+        
         // Todo have the constructor setup the questionlist object
         static public void Play()
         {
 
         }
         
-        internal void intro()
-        {
-            GUI.Print("Det her er en Quiz om emnet: ");
-            Thread.Sleep(3000);
-            // todo Add the Ability to see what the theme of the questions json file is about
-            Theme("Vandforbrug!");
 
-            // todo Add the ability to see how many questions there are
-            GUI.Print("Quizzen har 6 spørgsmål.");
-            Console.ReadKey(true);
-        }
-
-        /// <summary>
-        /// Gives the quiz theme the flicker
-        /// </summary>
-        /// <param name="name"></param>
-        static private void Theme(string name)
-        {
-            int height = Console.GetCursorPosition().Top, width = Console.GetCursorPosition().Left;
-
-            for (int i = 0; i < 15; i++)
-            {
-                GUI.Print(name, height, width, 100, ConsoleColor.Red);
-                GUI.Print(name, height, width, 100, ConsoleColor.Yellow);
-            }
-        }
     }
 }
